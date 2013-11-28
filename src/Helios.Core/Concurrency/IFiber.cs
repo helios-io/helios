98e34fdd49e08962d5f95e06205fa1c4abe9b13b
Aaron Stannard <aaron@markedup.com>
2013-11-27 18:05:41 -0800

    added a fiber implementation

--- src/Helios.Core/Concurrency/IFiber.cs
+++ src/Helios.Core/Concurrency/IFiber.cs
@@ -7,12 +7,17 @@ namespace Helios.Core.Concurrency
     /// </summary>
     public interface IFiber
     {
-
+        void Add(Action op);
 
         /// <summary>
         /// Shuts down this Fiber within the allotted timeframe
         /// </summary>
         /// <param name="gracePeriod">The amount of time given for currently executing tasks to complete</param>
         void Shutdown(TimeSpan gracePeriod);
+
+        /// <summary>
+        /// Performs a hard-stop on the Fiber - no more actions can be executed
+        /// </summary>
+        void Stop();
     }
 }
---
+++ src/Helios.Core/Concurrency/SynchronousFiber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Timers;
+using Helios.Core.Ops;
+
+namespace Helios.Core.Concurrency
+{
+    public class SynchronousFiber : IFiber
+    {
+        bool _acceptingRequests;
+
+        protected Timer ShutdownTimer;
+        protected readonly IExecutor Executor;
+
+        public SynchronousFiber(IExecutor executor)
+        {
+            _acceptingRequests = true;
+            Executor = executor;
+        }
+
+        public void Add(Action op)
+        {
+            if(_acceptingRequests)
+                op();
+        }
+
+        public void Shutdown(TimeSpan gracePeriod)
+        {
+            ShutdownTimer = new Timer(gracePeriod.TotalMilliseconds);
+
+            ShutdownTimer.Elapsed += (sender, args) =>
+            {
+                Stop();
+                ShutdownTimer.Stop();
+                ShutdownTimer.Dispose();
+                ShutdownTimer = null;
+            };
+        }
+
+        public void Stop()
+        {
+            _acceptingRequests = false;
+        }
+    }
+}
