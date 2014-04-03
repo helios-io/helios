2ff02921c97ae09055ceee91138b4c4b79d094e3
Aaron Stannard <aaron@markedup.com>
2014-04-02 19:05:49 -0700

    implemented the IChannelHandlerContext

--- src/Helios/Channels/IChannelHandler.cs
+++ src/Helios/Channels/IChannelHandler.cs
@@ -16,14 +16,14 @@ namespace Helios.Channels
         /// <summary>
         /// Called immediately after the <see cref="IChannelHandler"/> was added to the underlying <see cref="IChannel"/> and is ready to handle events.
         /// </summary>
-        /// <param name="context">Control context for this <see cref="IChannel"/></param>
-        void HandlerAdded(IChannelContext context);
+        /// <param name="handlerContext">Control handlerContext for this <see cref="IChannel"/></param>
+        void HandlerAdded(IChannelHandlerContext handlerContext);
 
         /// <summary>
         /// Called when the <see cref="IChannelHandler"/> is removed from the underlying <see cref="IChannel"/>.
         /// </summary>
-        /// <param name="context">Control context for this <see cref="IChannel"/></param>
-        void HandlerRemoved(IChannelContext context);
+        /// <param name="handlerContext">Control handlerContext for this <see cref="IChannel"/></param>
+        void HandlerRemoved(IChannelHandlerContext handlerContext);
 
         #endregion
 
@@ -32,34 +32,34 @@ namespace Helios.Channels
         /// <summary>
         /// Called if an Exception is thrown by the underlying <see cref="IChannel"/>
         /// </summary>
-        void ExceptionCaught(IChannelContext context, Exception ex);
+        void ExceptionCaught(IChannelHandlerContext handlerContext, Exception ex);
 
         /// <summary>
         /// Invoked when the this <see cref="IChannelHandler"/> completes registration inside the <see cref="IChannel"/> event loop
         /// </summary>
-        void ChannelRegistered(IChannelContext context);
+        void ChannelRegistered(IChannelHandlerContext handlerContext);
 
         /// <summary>
         /// Invoked when the underlying <see cref="IChannel"/> becomes active
         /// </summary>
-        void ChannelActive(IChannelContext context);
+        void ChannelActive(IChannelHandlerContext handlerContext);
 
         /// <summary>
         /// The channel has now reached the end of its lifetime and is no longer active;
         /// can be due to the remote endpoint shutting down or a number of other reasons.
         /// </summary>
-        void ChannelInactive(IChannelContext context);
+        void ChannelInactive(IChannelHandlerContext handlerContext);
 
         /// <summary>
         /// Received inbound data from the channel
         /// </summary>
-        void ChannelRead(IChannelContext context, object message);
+        void ChannelRead(IChannelHandlerContext handlerContext, object message);
 
         /// <summary>
         /// Gets called when the writable state of the underlying <see cref="IChannel"/> has changed. You can get the
         /// state by calling <see cref="IChannel.IsWritable"/>.
         /// </summary>
-        void ChannelWriteabilityChanged(IChannelContext context);
+        void ChannelWritabilityChanged(IChannelHandlerContext handlerContext);
 
         #endregion
 
@@ -68,49 +68,49 @@ namespace Helios.Channels
         /// <summary>
         /// Called once a bind operation is made to a local address. Typically used in combination with UDP.
         /// </summary>
-        /// <param name="context">The context used to make the binding operation</param>
+        /// <param name="handlerContext">The handlerContext used to make the binding operation</param>
         /// <param name="localAddress">The local address to bind on</param>
         /// <param name="bindCompletionSource">A task completion source to notify once the operation finishes</param>
-        void Bind(IChannelContext context, INode localAddress, TaskCompletionSource<bool> bindCompletionSource);
+        void Bind(IChannelHandlerContext handlerContext, INode localAddress, TaskCompletionSource<bool> bindCompletionSource);
 
         /// <summary>
         /// Called once a connect operation is made from a local address to a remote one. Typically used in combination with TCP.
         /// </summary>
-        /// <param name="context">The context used to make the connect operation.</param>
+        /// <param name="handlerContext">The handlerContext used to make the connect operation.</param>
         /// <param name="remoteAddress">The remote address to connect to.</param>
         /// <param name="localAddress">The local address to connect with.</param>
         /// <param name="connectCompletionSource">A task completion source to notify once the operation finishes.</param>
-        void Connect(IChannelContext context, INode remoteAddress, INode localAddress, TaskCompletionSource<bool> connectCompletionSource);
+        void Connect(IChannelHandlerContext handlerContext, INode remoteAddress, INode localAddress, TaskCompletionSource<bool> connectCompletionSource);
 
         /// <summary>
         /// Called once a disconnect operation is made. Typically used in combination with TCP.
         /// </summary>
-        /// <param name="context">The context used to make the disconnect operation.</param>
+        /// <param name="handlerContext">The handlerContext used to make the disconnect operation.</param>
         /// <param name="disconnectCompletionSource">A task completion source to notify once the operation finishes.</param>
-        void Disconnect(IChannelContext context, TaskCompletionSource<bool> disconnectCompletionSource);
+        void Disconnect(IChannelHandlerContext handlerContext, TaskCompletionSource<bool> disconnectCompletionSource);
 
         /// <summary>
         /// Called once a close operation is made.
         /// </summary>
-        /// <param name="context">The context used to make the close operation.</param>
+        /// <param name="handlerContext">The handlerContext used to make the close operation.</param>
         /// <param name="closeCompletionSource">A task completion source to notify once the operation finishes.</param>
-        void Close(IChannelContext context, TaskCompletionSource<bool> closeCompletionSource);
+        void Close(IChannelHandlerContext handlerContext, TaskCompletionSource<bool> closeCompletionSource);
 
         /// <summary>
         /// Called once a write operation is made. The write operation will write all messages directly onto the underlying
         /// <see cref="IChannel"/> and <see cref="IConnection"/> but may not be flushed to the network immediately.
         /// </summary>
-        /// <param name="context">The context used to make the write operation.</param>
-        /// <param name="message">The message being written by the <see cref="IChannelContext"/> to the <see cref="IChannel"/></param>
+        /// <param name="handlerContext">The handlerContext used to make the write operation.</param>
+        /// <param name="message">The message being written by the <see cref="IChannelHandlerContext"/> to the <see cref="IChannel"/></param>
         /// <param name="writeCompletionSource">A task completion source to notify once the operation finishes.</param>
-        void Write(IChannelContext context, object message, TaskCompletionSource<bool> writeCompletionSource);
+        void Write(IChannelHandlerContext handlerContext, object message, TaskCompletionSource<bool> writeCompletionSource);
 
         /// <summary>
         /// Called once a flush operation is made to the underlying <see cref="IConnection"/>. This operation tries to push any
         /// pending writes directly onto the network.
         /// </summary>
-        /// <param name="context">The contect used to make the flush operation.</param>
-        void Flush(IChannelContext context);
+        /// <param name="handlerContext">The contect used to make the flush operation.</param>
+        void Flush(IChannelHandlerContext handlerContext);
 
         #endregion
     }
---
+++ src/Helios/Channels/IChannelHandlerContext.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Threading.Tasks;
+using Helios.Ops;
+using Helios.Topology;
+
+namespace Helios.Channels
+{
+    /// <summary>
+    /// Exposes control and I/O capabilites to the methods implemented inside a <see cref="IChannelHandler"/>
+    /// </summary>
+    public interface IChannelHandlerContext
+    {
+        /// <summary>
+        /// Exposes the underlying <see cref="IChannel"/> that is bound to this context.
+        /// </summary>
+        IChannel Channel { get; }
+
+        /// <summary>
+        /// Returns the <see cref="IExecutor"/> used to invoke operations which occur on the <see cref="IChannelHandler"/>.
+        /// 
+        /// <remarks>
+        /// We strongly don't recommend calling this method unless you know what you're doing.
+        /// </remarks>
+        /// </summary>
+        IExecutor Invoker { get; }
+
+        /// <summary>
+        /// Provides a link to the underlying <see cref="IChannelHandler"/> that is bound to this <see cref="IChannelHandlerContext"/>.
+        /// </summary>
+        IChannelHandler Handler { get; }
+
+        /// <summary>
+        /// The unique name of this <see cref="IChannelHandlerContext"/>.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// A <see cref="IChannel"/> was regiestered to its event loop.
+        /// 
+        /// This will result in the <see cref="IChannelHandler.ChannelRegistered"/> method being called
+        /// </summary>
+        IChannelHandlerContext FireChannelRegistered();
+
+        /// <summary>
+        /// A <see cref="IChannel"/> is active now, which means it is connected.
+        /// 
+        /// This will result in the <see cref="IChannelHandler.ChannelActive"/> method being called.
+        /// </summary>
+        IChannelHandlerContext FireChannelActive();
+
+        /// <summary>
+        /// A <see cref="IChannel"/> is inactive now, which means it is closed.
+        /// 
+        /// This will result in the <see cref="IChannelHandler.ChannelInactive"/> method being called.
+        /// </summary>
+        IChannelHandlerContext FireChannelInactive();
+
+        /// <summary>
+        /// A <see cref="IChannel"/> received an <see cref="Exception"/> in one of its inbound operations.
+        /// 
+        /// This will result in having the <see cref="IChannelHandler.ExceptionCaught"/> called.
+        /// </summary>
+        IChannelHandlerContext FireExceptionCaught(Exception cause);
+
+        /// <summary>
+        /// A <see cref="IChannel"/> received a message.
+        /// 
+        /// This will result in having the <see cref="IChannelHandler.ChannelRead"/> method called.
+        /// </summary>
+        IChannelHandlerContext FireChannelRead(object message);
+
+        /// <summary>
+        /// Triggers a <see cref="IChannelHandler.ChannelWritabilityChanged"/> event on the <see cref="IChannelHandler"/>.
+        /// </summary>
+        /// <returns></returns>
+        IChannelHandlerContext FireChannelWritabilityChanged();
+
+        /// <summary>
+        /// Request to bind to a given <see cref="INode"/> address and notify the <see cref="Task{T}"/> once
+        /// the operation completes, either because the operation was successful or because of an error.
+        /// <remarks>
+        /// This will result in having the <see cref="IChannelHandler.Bind"/> method called.
+        /// </remarks>
+        /// </summary>
+        Task<bool> Bind(INode localAddress);
+
+        Task<bool> Bind(INode localAddress, TaskCompletionSource<bool> bindCompletionSource);
+
+        Task<bool> Connect(INode remoteAddress);
+
+        Task<bool> Connect(INode remoteAddress, TaskCompletionSource<bool> connectionCompletionSource);
+
+        Task<bool> Connect(INode remoteAddress, INode localAddress);
+
+        Task<bool> Connect(INode remoteAddress, INode localAddress, TaskCompletionSource<bool> connectCompletionSource);
+
+        Task<bool> Disconnect();
+
+        Task<bool> Disconnection(TaskCompletionSource<bool> disconnectCompletionSource);
+
+        Task<bool> Close();
+
+        Task<bool> Close(TaskCompletionSource<bool> closeCompletionSource);
+
+        IChannelHandlerContext Read();
+
+        Task<bool> Write(object message);
+
+        Task<bool> Write(object message, TaskCompletionSource<bool> writeCompletionSource);
+
+        IChannelHandlerContext Flush();
+
+        Task<bool> WriteAndFlush(object message, TaskCompletionSource<bool> writeCompletionSource);
+
+        Task<bool> WriteAndFlush(object message);
+
+        TaskCompletionSource<bool> NewCompletionSource();
+
+        Task<bool> NewSucceededTask();
+
+        Task<bool> NewFailedTask();
+    }
+}
