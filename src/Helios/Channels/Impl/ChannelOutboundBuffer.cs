f8a1338649579fad17d7f0c72d4dfc1e9955b30b
Aaron Stannard <aaron@markedup.com>
2014-04-06 14:56:46 -0700

    rewrote the ChannelOutboundBuffer

--- src/Helios/Channels/Impl/ChannelOutboundBuffer.cs
+++ src/Helios/Channels/Impl/ChannelOutboundBuffer.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
+using Helios.Exceptions;
 using Helios.Net;
 using Helios.Util;
 using Helios.Util.Collections;
@@ -13,25 +12,109 @@ namespace Helios.Channels.Impl
     /// </summary>
     public class ChannelOutboundBuffer
     {
-        public const int INITIAL_OUTBOUND_CAPACITY = 1000;
-        private ICircularBuffer<Entry> _messages = new ConcurrentCircularBuffer<Entry>(INITIAL_OUTBOUND_CAPACITY);
+        public const int INITIAL_OUTBOUND_CAPACITY = 100;
 
         protected ChannelOutboundBuffer(AbstractChannel channel)
         {
             Channel = channel;
+            buffer = new Entry[INITIAL_OUTBOUND_CAPACITY];
+            for (var i = 0; i < buffer.Length; i++)
+                buffer[i] = NewEntry();
         }
 
-        protected readonly AbstractChannel Channel;
-        private bool inFail;
+        protected AbstractChannel Channel;
+        private volatile bool inFail;
+        private Entry[] buffer;
+        private int flushed;
+        private int unflushed;
+        private int tail;
+
         private AtomicReference<int> writable = 1;
         private AtomicReference<long> totalPendingSize = 0;
+
+        protected Entry[] entries
+        {
+            get { return buffer; }
+        }
+
+        public void AddMessage(NetworkData message, ChannelPromise<bool> promise)
+        {
+            int size = message.Length;
+            if (size < 0) size = 0;
+
+            Entry e = buffer[tail++];
+            e.Message = message.Buffer;
+            e.PendingSize = size;
+            e.Promise = promise;
+
+            tail &= buffer.Length - 1;
+
+            if (tail == flushed)
+            {
+                AddCapacity();
+            }
+
+            IncrementPendingOutboundBytes(size);
+        }
+
+        /// <summary>
+        /// Expand the internal buffer
+        /// </summary>
+        private void AddCapacity()
+        {
+            var p = flushed;
+            var n = buffer.Length;
+            var r = n - p; // number of elements to the right of p
+            var s = Size;
+
+            var newCapacity = n << 1;
+            if (newCapacity < 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var e = new Entry[newCapacity];
+            Array.Copy(buffer, p, e, 0, r);
+            Array.Copy(buffer, 0, e, r, p);
+            for (var i = n; i < e.Length; i++)
+            {
+                e[i] = NewEntry();
+            }
+
+            buffer = e;
+            flushed = 0;
+            unflushed = s;
+            tail = n;
+        }
+
+        /// <summary>
+        /// Mark all messages in this <see cref="ChannelOutboundBuffer"/> as flushed
+        /// </summary>
+        public void AddFlush()
+        {
+            unflushed = tail;
+
+            var mask = buffer.Length - 1;
+            var i = flushed;
+            while (i != unflushed && buffer[i].Message != null)
+            {
+                var entry = buffer[i];
+                if (entry.Promise.Task.IsCanceled)
+                {
+                    // Was cancelled, so make sure we free up memory and notify about the freed bytes
+                    var pending = entry.Cancel();
+                    DecrementPendingOutboundBytes(pending);
+                }
+                i = i + 1 & mask;
+            }
+        }
 
         /// <summary>
         /// Increment the pending bytes which will be written at some point
         /// </summary>
         public void IncrementPendingOutboundBytes(int size)
         {
-            var channel = this.Channel;
+            var channel = Channel;
             if (size == 0 || channel == null) return;
 
             long oldValue = totalPendingSize;
@@ -42,27 +125,211 @@ namespace Helios.Channels.Impl
                 newWriteBufferSize = oldValue + size;
             }
 
+            var highWaterMark = channel.Config.WriteBufferHighWaterMark;
 
-        }
-
-        internal ChannelFuture<bool> AddWrite(NetworkData message)
-        {
-            var promise = NewPromise();
-            if (_messages.Size == _messages.Capacity) //cancel the message at the front of the buffer since we weren't able to deliver it on time
+            if (newWriteBufferSize > highWaterMark)
             {
-                var cancelledHead = _messages.Dequeue();
-                cancelledHead.Cancel();
+                if (writable.CompareAndSet(1, 0))
+                {
+                    channel.Pipeline.FireChannelWritabilityChanged();
+                }
             }
-            _messages.Enqueue(NewEntry(message, promise));
-            return promise.Task;
         }
 
         /// <summary>
-        /// Flush all of the messages in this buffer
+        /// Decrement the pending bytes which will be written at some point
         /// </summary>
-        internal IList<Entry> Flush()
+        public void DecrementPendingOutboundBytes(int size)
         {
-            return _messages.DequeueAll().ToList();
+            var channel = this.Channel;
+            if (size == 0 || channel == null) return;
+
+            long oldValue = totalPendingSize;
+            var newWriteBufferSize = oldValue - size;
+            while (!totalPendingSize.CompareAndSet(oldValue, newWriteBufferSize))
+            {
+                oldValue = totalPendingSize;
+                newWriteBufferSize = oldValue - size;
+            }
+
+            int lowWaterMark = channel.Config.WriteBufferLowWaterMark;
+
+            if (newWriteBufferSize == 0 || newWriteBufferSize < lowWaterMark)
+            {
+                if (writable.CompareAndSet(0,1))
+                {
+                    channel.Pipeline.FireChannelWritabilityChanged();
+                }
+            }
+        }
+
+        public byte[] Current
+        {
+            get
+            {
+                if (IsEmpty) return null;
+                var entry = buffer[flushed];
+                return entry.Message;
+            }
+        }
+
+        /// <summary>
+        /// Mark the current message as successful and remove it from this <see cref="ChannelOutboundBuffer"/>
+        /// </summary>
+        /// <returns>true if there are more messages left to process, false otherwise</returns>
+        public bool Remove()
+        {
+            if (IsEmpty) return false;
+            var e = buffer[flushed];
+            if (e == null || e.Message == null) return false;
+
+            var promise = e.Promise;
+            var size = e.PendingSize;
+
+            e.Clear();
+
+            flushed = flushed + 1 & buffer.Length - 1;
+
+            if (!e.Cancelled)
+            {
+                //only release message, notify, and decrement if it was not canceled before
+                SafeSuccess(e.Promise);
+                DecrementPendingOutboundBytes(size);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Mark the current message as unsuccessful and remove it from this <see cref="ChannelOutboundBuffer"/>.
+        /// </summary>
+        /// <returns>true if there are more messages left to process, false otherwise</returns>
+        public bool Remove(Exception cause)
+        {
+            if (IsEmpty) return false;
+            var e = buffer[flushed];
+            if (e == null || e.Message == null) return false;
+
+            var promise = e.Promise;
+            var size = e.PendingSize;
+
+            e.Clear();
+
+            flushed = flushed + 1 & buffer.Length - 1;
+
+            if (!e.Cancelled)
+            {
+                //only release message, notify, and decrement if it was not canceled before
+                SafeFail(promise, cause);
+                DecrementPendingOutboundBytes(size);
+            }
+
+            return true;
+        }
+
+        public int Size { get { return unflushed - flushed & buffer.Length - 1; } }
+
+        public bool IsEmpty { get { return unflushed == flushed; } }
+
+        /// <summary>
+        /// Fail all unflushed messages with the given <see cref="cause"/>
+        /// </summary>
+        public void FailFlushed(Exception cause)
+        {
+            if (inFail) return; //don't re-enter this method
+
+            try
+            {
+                inFail = true;
+                for (;;)
+                {
+                    if (!Remove(cause)) break;
+                }
+            }
+            finally
+            {
+                inFail = false;
+            }
+        }
+
+        /// <summary>
+        /// Fail all pending messages with the given <see cref="HeliosConnectionException"/>
+        /// </summary>
+        public void Close(HeliosConnectionException cause)
+        {
+            if (inFail)
+            { 
+                //schedule to run later
+                Channel.EventLoop.Execute(() => Close(cause));
+                return;
+            }
+
+            inFail = true;
+
+            if (Channel.IsOpen)
+            {
+                throw new InvalidOperationException("Close() must be invoked after the channel is closed.");
+            }
+
+            if (!IsEmpty)
+            {
+                throw new InvalidOperationException("Close() must be invoked after all flushed writes are handled.");
+            }
+
+            //Release all unflushed messages
+            var unflushedCount = tail - unflushed & buffer.Length - 1;
+            try
+            {
+                for (var i = 0; i < unflushedCount; i++)
+                {
+                    var e = buffer[unflushed + i & buffer.Length - 1];
+
+                    //Just a decrease; do not trigger any events via decrementPendingOutboundBytes()
+                    var size = e.PendingSize;
+                    long oldValue = totalPendingSize;
+                    long newWriteBufferSize = oldValue - size;
+                    while (!totalPendingSize.CompareAndSet(oldValue, newWriteBufferSize))
+                    {
+                        oldValue = totalPendingSize;
+                        newWriteBufferSize = oldValue - size;
+                    }
+
+                    e.PendingSize = 0;
+                    if (!e.Cancelled)
+                    {
+                        SafeFail(e.Promise, cause);
+                    }
+                    e.Message = null;
+                    e.Promise = null;
+                }
+            }
+            finally
+            {
+                tail = unflushed;
+                inFail = false;
+            }
+            Recycle();
+        }
+
+        private void Recycle()
+        {
+            if (buffer.Length > INITIAL_OUTBOUND_CAPACITY)
+            {
+                var e = new Entry[INITIAL_OUTBOUND_CAPACITY];
+                Array.Copy(buffer, 0, e, 0, INITIAL_OUTBOUND_CAPACITY);
+                buffer = e;
+            }
+
+            flushed = 0;
+            unflushed = 0;
+            tail = 0;
+
+            // Set the channel to null so it can be GC'ed ASAP
+            Channel = null;
+
+            totalPendingSize = 0;
+            writable = 1;
+
         }
 
         internal ChannelPromise<bool> NewPromise()
@@ -70,9 +337,14 @@ namespace Helios.Channels.Impl
             return new ChannelPromise<bool>(Channel);
         }
 
-        internal static Entry NewEntry(NetworkData message, ChannelPromise<bool> flushPromise)
+        static Entry NewEntry(NetworkData message, ChannelPromise<bool> flushPromise)
         {
-            return new Entry(){ Message = message.Buffer, FlushedCompletionSource = flushPromise, PendingSize = message.Length};
+            return new Entry(){ Message = message.Buffer, Promise = flushPromise, PendingSize = message.Length};
+        }
+
+        static Entry NewEntry()
+        {
+            return new Entry();
         }
 
         #region Static methods
@@ -85,12 +357,28 @@ namespace Helios.Channels.Impl
             return new ChannelOutboundBuffer(channel);
         }
 
+        internal static void SafeSuccess(ChannelPromise<bool> promise)
+        {
+            if (!(promise is VoidChannelPromise) && !promise.TrySetResult(true))
+            {
+                //add logging here
+            }
+        }
+
+        internal static void SafeFail(ChannelPromise<bool> promise, Exception cause)
+        {
+            if (!(promise is VoidChannelPromise) && !promise.TrySetException(cause))
+            {
+                //add logging here
+            }
+        }
+
         #endregion
 
-        internal class Entry
+        protected class Entry
         {
             public byte[] Message { get; set; }
-            public ChannelPromise<bool> FlushedCompletionSource { get; set; }
+            public ChannelPromise<bool> Promise { get; set; }
 
             /// <summary>
             /// The size of this message in bytes
@@ -109,7 +397,7 @@ namespace Helios.Channels.Impl
                     Cancelled = true;
                     var pSize = PendingSize;
                     Message = Unpooled.EmptyBuffer;
-                    FlushedCompletionSource.SetCanceled();
+                    Promise.SetCanceled();
                     return pSize;
                 }
 
@@ -123,7 +411,7 @@ namespace Helios.Channels.Impl
             public void Clear()
             {
                 Cancelled = true;
-                FlushedCompletionSource = null;
+                Promise = null;
                 PendingSize = 0;
                 Message = null;
             }
