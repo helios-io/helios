4d9e9c0f8f826bcc873bb196c56082ed55303340
Aaron Stannard <aaron@markedup.com>
2014-04-09 17:12:10 -0700

    added some tests to validate that we can change exception handlers at runtime

--- src/Helios/Reactor/ProxyReactorBase.cs
+++ src/Helios/Reactor/ProxyReactorBase.cs
@@ -31,27 +31,4 @@ namespace Helios.Reactor
             responseChannel.OnReceive(availableData);
         }
     }
-
-    /// <summary>
-    /// <see cref="IReactor"/> implementation which spawns <see cref="ReactorProxyResponseChannel"/> instances for responding directly with connected clients,
-    /// but maintains a single event loop for responding to incoming requests, rather than allowing each <see cref="ReactorProxyResponseChannel"/> to maintain
-    /// its own independent event loop.
-    /// 
-    /// Great for scenarios where you want to be able to set a single event loop for a server
-    /// </summary>
-    public abstract class SingleReceiveLoopProxyReactor<TIdentifier> : ProxyReactorBase<TIdentifier>
-    {
-        protected SingleReceiveLoopProxyReactor(IPAddress localAddress, int localPort, NetworkEventLoop eventLoop, SocketType socketType = SocketType.Stream, ProtocolType protocol = ProtocolType.Tcp, int bufferSize = NetworkConstants.DEFAULT_BUFFER_SIZE) 
-            : base(localAddress, localPort, eventLoop, socketType, protocol, bufferSize)
-        {
-        }
-
-        protected override void ReceivedData(NetworkData availableData, ReactorResponseChannel responseChannel)
-        {
-            if (EventLoop.Receive != null)
-            {
-                EventLoop.Receive(availableData, responseChannel);
-            }
-        }
-    }
 }
---
+++ tests/Helios.Tests/Ops/NetworkEventLoopTests.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Helios.Ops;
+using Helios.Util;
+using NUnit.Framework;
+
+namespace Helios.Tests.Ops
+{
+    [TestFixture]
+    public class NetworkEventLoopTests
+    {
+        #region Setup / Teardown
+        #endregion
+
+        #region Tests
+        
+        [Test]
+        public void Should_be_able_to_change_NetworkEventLoop_error_handler_at_runtime()
+        {
+            var eventLoop = EventLoopFactory.CreateNetworkEventLoop();
+            var count = new AtomicCounter(0);
+            var trappedException = false;
+            var backgroundProducer = Task.Run(() =>
+            {
+
+                for (var i = 0; i < 10; i++)
+                {
+                    eventLoop.Execute(() => count.GetAndIncrement());
+                    Thread.Sleep(10);
+                }
+            });
+
+            eventLoop.SetExceptionHandler((connection, exception) => trappedException = true, null);
+            eventLoop.Execute(() =>
+            {
+                throw new Exception("I'm an exception!");
+            });
+
+            backgroundProducer.Wait();
+
+            Assert.AreEqual(10, count.Current);
+            Assert.IsTrue(trappedException);
+        }
+
+        #endregion
+    }
+}
