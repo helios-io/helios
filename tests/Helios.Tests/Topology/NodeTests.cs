a3a421968c3b1fa1495a8f8943b0168e9674e088
Aaron Stannard <aaron@markedup.com>
2014-04-10 20:53:13 -0700

    added the tests file back

---
+++ tests/Helios.Tests/Topology/NodeTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using Helios.Topology;
+using NUnit.Framework;
+
+namespace Helios.Tests.Topology
+{
+    [TestFixture]
+    public class NodeTests
+    {
+        #region Setup / Teardown
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void Should_resolve_localhost_hostname_to_IP()
+        {
+            //arrange
+            var testNode =
+               NodeBuilder.BuildNode().Host("localhost").WithPort(1337).WithTransportType(TransportType.Udp);
+
+            var expectNode =
+                NodeBuilder.BuildNode().Host(IPAddress.Loopback).WithPort(1337).WithTransportType(TransportType.Udp);
+
+            //act
+
+            //assert
+            Assert.AreEqual(expectNode, testNode);
+        }
+
+        [Test]
+        public void Should_resolve_remote_hostname_to_IP()
+        {
+            //arrange
+            var testNode =
+                NodeBuilder.BuildNode().Host("yahoo.com").WithPort(80).WithTransportType(TransportType.Tcp);
+
+            //act
+
+            //assert
+            Assert.IsNotNull(testNode.Host);
+        }
+
+        #endregion
+    }
+}
