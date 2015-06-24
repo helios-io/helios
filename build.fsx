8a1f22bae849109c34591a14ef4bb9f159e43bb0
Aaron Stannard <aaron@petabridge.com>
2015-06-24 16:20:06 -0700

    fixed NuGet file copy issue

--- build.fsx
+++ build.fsx
@@ -159,19 +159,25 @@ Target "CleanNuget" (fun _ ->
 // Pack nuget for all projects
 // Publish to nuget.org if nugetkey is specified
 
-
 let createNugetPackages _ =
+    let mutable dirName = 1
     let removeDir dir = 
         let del _ = 
             DeleteDir dir
             not (directoryExists dir)
         runWithRetries del 3 |> ignore
 
+    let getDirName workingDir dirCount =
+        workingDir + dirCount.ToString()
+
+    CleanDir workingDir
+
     ensureDirectory nugetDir
     for nuspec in !! "src/**/*.nuspec" do
+        let ourWorkingDir = getDirName workingDir dirName
         printfn "Creating nuget packages for %s" nuspec
         
-        CleanDir workingDir
+        ensureDirectory ourWorkingDir
 
         let project = Path.GetFileNameWithoutExtension nuspec 
         let projectDir = Path.GetDirectoryName nuspec
@@ -197,7 +203,7 @@ let createNugetPackages _ =
                         Version = releaseVersion
                         Tags = tags |> String.concat " "
                         OutputPath = outputDir
-                        WorkingDir = workingDir
+                        WorkingDir = ourWorkingDir
                         SymbolPackage = symbolPackage
                         Dependencies = dependencies })
                 nuspec
@@ -207,7 +213,7 @@ let createNugetPackages _ =
             printfn "Handling nuget for %s" bin
             let releaseDir = "src" @@ bin @@ "bin" @@ "Release"
             printfn "Copying binaries from %s" releaseDir
-            let libDir = workingDir @@ getProjectBinFolders bin
+            let libDir = ourWorkingDir @@ getProjectBinFolders bin
             printfn "Creating output directory %s" libDir
             ensureDirectory libDir
             !! (releaseDir @@ bin + ".dll")
@@ -216,7 +222,7 @@ let createNugetPackages _ =
             |> CopyFiles libDir
 
         // Copy all src-files (.cs and .fs files) to workingDir/src
-        let nugetSrcDir = workingDir @@ @"src/"
+        let nugetSrcDir = ourWorkingDir @@ @"src/"
         // CreateDir nugetSrcDir
 
         let isCs = hasExt ".cs"
@@ -233,7 +239,8 @@ let createNugetPackages _ =
         // Uses the files we copied to workingDir and outputs to nugetdir
         pack nugetDir NugetSymbolPackage.Nuspec
         
-        removeDir workingDir
+        //removeDir ourWorkingDir
+        dirName <- dirName + 1
 
 let publishNugetPackages _ = 
     let rec publishPackage url accessKey trialsLeft packageFile =
