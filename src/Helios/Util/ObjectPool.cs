2d5baa3f9d396c40a424162615effccd4ca744cf
Aaron Stannard <aaron@petabridge.com>
2016-04-19 14:32:30 -0700

    working on object pooling

---
+++ src/Helios/Util/ObjectPool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helios.Util
+{
+    /// <summary>
+    /// Pooling implementation for reusable objects
+    /// </summary>
+    public sealed class ObjectPool<T> : IDisposable
+    {
+        abstract class ItemStore<T>
+    }
+}
