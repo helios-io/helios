748876be1ddf0b23ad439963e4fae86d27da1fcb
Aaron Stannard <aaron@petabridge.com>
2016-04-20 17:01:40 -0700

    added property-based tests for ChannelOutboundBuffer

--- tests/Helios.FsCheck.Tests/Channels/ChannelOutboundBufferSpecs.cs
+++ tests/Helios.FsCheck.Tests/Channels/ChannelOutboundBufferSpecs.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 using FsCheck;
 using FsCheck.Xunit;
@@ -10,6 +8,7 @@ using Helios.Buffers;
 using Helios.Channels;
 using Helios.Concurrency;
 using Helios.FsCheck.Tests.Buffers;
+using Helios.Util;
 
 namespace Helios.FsCheck.Tests.Channels
 {
@@ -29,7 +28,6 @@ namespace Helios.FsCheck.Tests.Channels
         [Property(QuietOnSuccess = true, MaxTest = 10000)]
         public Property ChannelOutboundBuffer_must_always_correctly_report_writability_and_PendingBytes(IByteBuf[] writes)
         {
-            var totalExpectedSize = writes.Sum(w => w.ReadableBytes);
             bool writeable = true;
             var buffer = new ChannelOutboundBuffer(WriteHighWaterMark, WriteLowWaterMark, () =>
             {
@@ -85,6 +83,112 @@ namespace Helios.FsCheck.Tests.Channels
             }
 
             return true.ToProperty();
+        }
+
+        [Property(QuietOnSuccess = true, StartSize = 2, MaxTest = 5000)]
+        public Property ChannelOutboundBuffer_must_always_process_pending_writes_in_FIFO_order(IByteBuf[] writes)
+        {
+            if (writes.Length == 0) // skip any zero-length results
+                return true.ToProperty(); 
+            bool writeable = true;
+            var buffer = new ChannelOutboundBuffer(WriteHighWaterMark, WriteLowWaterMark, () =>
+            {
+                writeable = !writeable; // toggle writeability
+            });
+
+            foreach (var message in writes)
+            {
+                buffer.AddMessage(message, message.ReadableBytes, TaskCompletionSource.Void);
+            }
+            buffer.AddFlush(); // have to flush in order to put messages into a readable state
+
+            var comparer = BufferOperations.Comparer;
+            var iterator = writes.GetEnumerator();
+            iterator.MoveNext();
+            var position = 0;
+            do
+            {
+                var read = buffer.Current as IByteBuf;
+                var match = comparer.Equals(read, iterator.Current);
+                if (!match)
+                    return
+                        false.Label(
+                            $"ChannelOutboundBuffer item at position {position} did not match the expected value.");
+                position++;
+            } while (buffer.Remove() && iterator.MoveNext());
+
+            return true.ToProperty();
+        }
+
+        [Property(QuietOnSuccess = true, MaxTest = 10000)]
+        public Property ChannelOutboundBuffer_must_complete_all_promises_successfully_on_read_regardless_of_flush_order(IByteBuf[] writes)
+        {
+            var tasks = new List<Task>();
+            bool writeable = true;
+            var buffer = new ChannelOutboundBuffer(WriteHighWaterMark, WriteLowWaterMark, () =>
+            {
+                writeable = !writeable; // toggle writeability
+            });
+
+            // write + flush loop
+            foreach (var message in writes)
+            {
+                var tcs = new TaskCompletionSource();
+                tasks.Add(tcs.Task);
+                buffer.AddMessage(message, message.ReadableBytes, tcs);
+                if(ThreadLocalRandom.Current.Next(0,1) == 0) // just doing this to guarantee we don't screw up the linked list
+                    buffer.AddFlush();
+            }
+            buffer.AddFlush(); // add a final flush in case RNG hasn't let us do one in a while
+
+            // sanity check
+            if (buffer.Count != writes.Length)
+                return
+                    false.Label(
+                        $"Expected buffer to have {writes.Length} writes waiting for processing; found {buffer.Count} instead.");
+
+            var completion = Task.WhenAll(tasks);
+            if (!tasks.TrueForAll(x => x.IsCompleted == false))
+                return
+                    false.Label(
+                        "None of the messages have been processed, but still found Tasks reporting successful completion");
+
+            do
+            {
+                // process all of the messages
+            } while (buffer.Remove());
+
+            return completion.IsCompleted.Label("Expected all underlying tasks to be completed.");
+        }
+
+        [Property(QuietOnSuccess = true)]
+        public Property ChannelOutboundBuffer_must_dump_all_flushed_messages_upon_failure(IByteBuf[] writes)
+        {
+            var tasks = new List<Task>();
+            bool writeable = true;
+            var buffer = new ChannelOutboundBuffer(WriteHighWaterMark, WriteLowWaterMark, () =>
+            {
+                writeable = !writeable; // toggle writeability
+            });
+
+            // write
+            foreach (var message in writes)
+            {
+                var tcs = new TaskCompletionSource();
+                tasks.Add(tcs.Task);
+                buffer.AddMessage(message, message.ReadableBytes, tcs);
+            }
+            buffer.AddFlush(); // flush em
+
+            var completion = Task.WhenAll(tasks);
+
+            // err
+            var exception = new ApplicationException("TEST");
+            buffer.FailFlushed(exception, true);
+
+            return (completion.IsFaulted && buffer.IsWritable && tasks.All(x => x.IsFaulted && x.Exception.InnerExceptions.Contains(exception)))
+                .When(writes.Length > 0)
+                .Label("Expected all flushed messages to be faulted with the same exception upon FailFlush, and for buffer to be writable.");
         }
     }
 }
