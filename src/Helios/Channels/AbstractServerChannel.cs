c348a99c3054a900cb0ba48a9afb1cbe73997e09
Aaron Stannard <aaron@petabridge.com>
2016-04-25 19:05:09 -0700

    implemented AbstractServerChannel

---
+++ src/Helios/Channels/AbstractServerChannel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Helios.Concurrency;
+using Helios.Util.Concurrency;
+
+namespace Helios.Channels
+{
+    /// <summary>
+    /// A skeleton of a server-side <see cref="IChannel"/> implementation, which
+    /// does not allow any of the following operations:
+    /// 
+    /// * <see cref="IChannel.ConnectAsync(EndPoint)"/>
+    /// * <see cref="IChannel.DisconnectAsync()"/>
+    /// * <see cref="IChannel.WriteAsync(object)"/>
+    /// * <see cref="IChannel.Flush()"/>
+    /// </summary>
+    public abstract class AbstractServerChannel : AbstractChannel, IServerChannel
+    {
+        protected AbstractServerChannel() : base(null)
+        {
+        }
+
+        protected override EndPoint RemoteAddressInternal { get { return null; } }
+
+        protected override void DoDisconnect()
+        {
+            throw new NotSupportedException();
+        }
+
+        protected override void DoWrite(ChannelOutboundBuffer input)
+        {
+            throw new NotSupportedException();
+        }
+
+        protected override object FilterOutboundMessage(object msg)
+        {
+            throw new NotSupportedException();
+        }
+
+        protected override IChannelUnsafe NewUnsafe()
+        {
+            return new DefaultServerUnsafe(this);
+        }
+
+        sealed class DefaultServerUnsafe : AbstractUnsafe
+        {
+            public DefaultServerUnsafe(AbstractChannel channel) : base(channel)
+            {
+            }
+
+            public override Task ConnectAsync(EndPoint remoteAddress, EndPoint localAddress)
+            {
+                return TaskEx.FromException(new NotSupportedException());
+            }
+        }
+    }
+}
