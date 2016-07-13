f906a9473c4d0e202b9bcf0e1edc3f0dad22e7e7
Aaron Stannard <aaron@petabridge.com>
2016-07-13 15:26:54 -0700

    added specifier for DNS resolution

--- src/Helios/Channels/Bootstrap/DefaultNameResolver.cs
+++ src/Helios/Channels/Bootstrap/DefaultNameResolver.cs
@@ -2,7 +2,9 @@
 // Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
 // See ThirdPartyNotices.txt for references to third party code used inside Helios.
 
+using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace Helios.Channels.Bootstrap
@@ -21,6 +23,17 @@ namespace Helios.Channels.Bootstrap
             {
                 var resolved = await Dns.GetHostEntryAsync(asDns.Host);
                 return new IPEndPoint(resolved.AddressList[0], asDns.Port);
+            }
+            return address;
+        }
+
+        public async Task<EndPoint> ResolveAsync(EndPoint address, AddressFamily preferredFamily)
+        {
+            var asDns = address as DnsEndPoint;
+            if (asDns != null)
+            {
+                var resolved = await Dns.GetHostEntryAsync(asDns.Host);
+                return new IPEndPoint(resolved.AddressList.First(x => x.AddressFamily == preferredFamily), asDns.Port);
             }
             return address;
         }
